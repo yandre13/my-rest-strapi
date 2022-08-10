e66513eff46e4b20b8df65e2786812bad9adb57c
yaser <yandre13.elsf@gmail.com>
2022-08-10 10:37:50 -0500

    feat: add cloudinary for images

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,30 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", "https:"],
+          'img-src': [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://res.cloudinary.com",
+          ],
+          'media-src': ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
